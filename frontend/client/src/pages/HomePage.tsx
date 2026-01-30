46623a17687e2f03afeb0f9c416dbe2191120168
Caue Casonato <caueroriz@gmail.com>
2026-01-29 18:42:06 -0800

    Check again

--- frontend/client/src/pages/HomePage.tsx
+++ frontend/client/src/pages/HomePage.tsx
@@ -39,7 +39,7 @@ export default function HomePage() {
   ?.filter(event => new Date(event.date) > new Date())
   .filter(
     (event) =>
-      event.title !== 'Meeting CDPI 2026'
+      event.title !== 'Workshop 360º na indústria farmacêutica - Montes Claros'
   )
   ?.sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime());
 
