51a1ce3c66913bfbf44a89f7425bba4914f7aaeb
Caue Casonato <caueroriz@gmail.com>
2026-03-25 18:02:38 -0700

    FIX: Unused event hidden

--- frontend/client/src/pages/EventsPage.tsx
+++ frontend/client/src/pages/EventsPage.tsx
@@ -28,7 +28,7 @@ export default function EventsPage() {
     })
     .filter(
       (event) =>
-        event.title !== 'Workshop Eficácia e Segurança de Medicamentos: Comprovação de Eficácia e Aspectos Técnicos para Medicamentos de Liberação Prolongada'
+        event.title !== 'Meeting CDPI 2026'
     )
     ?.sort((a, b) => +new Date(a.date) - +new Date(b.date)); // Sort by date ascending
 
